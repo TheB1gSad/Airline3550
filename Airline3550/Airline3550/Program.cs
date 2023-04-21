afeba05c84522d3b85c325a535a1a17701112299
knzeitouni <karam.zeitouni@gmail.com>
2023-04-21 00:06:54 -0400

    Put main back to original state

--- Airline3550/Airline3550/Program.cs
+++ Airline3550/Airline3550/Program.cs
@@ -13,29 +13,8 @@ namespace Airline3550
 		{
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            //ApplicationConfiguration.Initialize();
-            //Application.Run(new LoginScreen());
-            List<Airport> airports = AirportGraph.createAirportGraph();
-            using (var writer = new StreamWriter("flightList.csv"))
-            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-            {
-                csv.WriteRecords(airports[0].flights);
-            }
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                // Don't write the header again.
-                HasHeaderRecord = false,
-            };
-            using (var stream = File.Open("flightList.csv", FileMode.Append))
-            using (var writer = new StreamWriter(stream))
-            using (var csv = new CsvWriter(writer, config))
-            {
-                for (int i = 1; i < airports.Count; i++)
-                {
-                    csv.WriteRecords(airports[i].flights);
-                }
-            }
-
+            ApplicationConfiguration.Initialize();
+            Application.Run(new LoginScreen());
         }
 	}
 }
