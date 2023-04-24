a1e94ff12367d98ff8168a7ef2e5dd56bc80081e
knzeitouni <karam.zeitouni@gmail.com>
2023-04-23 21:04:25 -0400

    FlightManager class created

---
+++ Airline3550/Airline3550/FlightManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+using CsvHelper;
+using System.Globalization;
+
+namespace Airline3550
+{
+    internal class FlightManager
+    {
+        public List<string> seatList(int flightID)
+        {
+            List<string> seatList = new List<string>();
+            string filePathSeats = Path.GetDirectoryName(Application.ExecutablePath);
+            string fileCsvSeats = Path.Combine(filePathSeats, "..", "..", "..", "csv", "flightSeats.csv");
+            using (var streamSeatsReader = File.OpenText("flightSeats.csv"))
+            {
+                string line;
+                while ((line = streamSeatsReader.ReadLine()) != null)
+                {
+                    if (Convert.ToInt32(line.Split(',')[0]) == flightID)
+                    {
+                        string[] seats = line.Split(',');
+                        seatList = seats.ToList();
+                        break;
+                    }
+
+                } 
+            }
+                return seatList;
+        }
+    }
+}
