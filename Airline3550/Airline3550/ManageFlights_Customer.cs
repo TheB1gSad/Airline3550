1fc67c5feeb629fcff8fde8ace45d7220a93912b
TheB1gSad <cfinetti@rockets.utoledo.edu>
2023-04-24 13:02:28 -0400

    Fixed manage flights to update with the table of flights with a booking if the user books a flight this session.

--- Airline3550/Airline3550/ManageFlights_Customer.cs
+++ Airline3550/Airline3550/ManageFlights_Customer.cs
@@ -12,124 +12,154 @@ using System.Xml;
 
 namespace Airline3550
 {
-	public partial class ManageFlights_Customer : Form
-	{
-		List<string> currentFlights = new List<string>();
-		List<string> previousFlights = new List<string>();
-		List<string[]> userTransactions = new List<string[]>();
-		static string executablePath = AppDomain.CurrentDomain.BaseDirectory;
-		private static string flightListPath = Path.Combine(executablePath, "flightList.csv");
-		private static string transactionlistPath = Path.Combine(executablePath, "userTransactions.csv");
-		private CustomerMenu mainMenu;
-		public ManageFlights_Customer(CustomerMenu menu)
-		{
-			mainMenu = menu;
-			InitializeComponent();
-			initializeFlightList();
+    public partial class ManageFlights_Customer : Form
+    {
+        List<string> currentFlights = new List<string>();
+        List<string> previousFlights = new List<string>();
+        List<string[]> userTransactions = new List<string[]>();
+        static string executablePath = AppDomain.CurrentDomain.BaseDirectory;
+        private static string flightListPath = Path.Combine(executablePath, "flightList.csv");
+        private static string transactionlistPath = Path.Combine(executablePath, "userTransactions.csv");
+        private CustomerMenu mainMenu;
+        public ManageFlights_Customer(CustomerMenu menu)
+        {
+            mainMenu = menu;
+            InitializeComponent();
+            //initializeFlightList();
 
-		}
+        }
 
-		private void initializeFlightList()
-		{
-
-
-
-			//Read every flight from list
-			string line;
-			StreamReader streamReaderA = new StreamReader(transactionlistPath);
-			//Read in every transaction line by line, find all that have the user id of this user add to transaction list
-			while ((line = streamReaderA.ReadLine()) != null)
-			{
-				string[] currentLine = line.Split(',');
-				if (currentLine[0] == mainMenu.userData.userName)
-				{
-					userTransactions.Add(currentLine);
-				}
-			}
+        private void initializeFlightList()
+        {
 
 
-			streamReaderA.Close();
 
-			for (int i = 0; i < userTransactions.Count; i++)
-			{
-				streamReaderA = new StreamReader(flightListPath);
-				while ((line = streamReaderA.ReadLine()) != null)
-				{
-					string[] currentLine = line.Split(',');
+            //Read every flight from list
+            string line;
+            StreamReader streamReaderA = new StreamReader(transactionlistPath);
+            //Read in every transaction line by line, find all that have the user id of this user add to transaction list
+            while ((line = streamReaderA.ReadLine()) != null)
+            {
+                string[] currentLine = line.Split(',');
+                if (currentLine[0] == mainMenu.userData.userName)
+                {
+                    userTransactions.Add(currentLine);
+                }
+            }
 
-					if (currentLine[3] != userTransactions.ToArray()[i][4])
-					{
-						continue;
-					}
-					DateTime flightDateTime = DateTime.Parse(currentLine[2] + " " + currentLine[0]);
-					//Flight must have already happened
-					if (flightDateTime < DateTime.Now)
-					{
-						previousFlights.Add(String.Join(',', currentLine));
-					}
-					//Flight Is Upcoming
-					else
-					{
-						currentFlights.Add(String.Join(',', currentLine));
-					}
-					break;
 
-				}
-				streamReaderA.Close();
-			}
-			addPreviousFlight(previousFlights);
-			addUpcomingFlight(currentFlights);
-			streamReaderA.Close();
-		}
+            streamReaderA.Close();
 
-		private void addPreviousFlight(List<string> flights)
-		{
+            for (int i = 0; i < userTransactions.Count; i++)
+            {
+                streamReaderA = new StreamReader(flightListPath);
+                while ((line = streamReaderA.ReadLine()) != null)
+                {
+                    string[] currentLine = line.Split(',');
 
-			RowStyle tempor = previousFlightsTable.RowStyles[0];
-			previousFlightsTable.RowStyles.Add(new RowStyle(tempor.SizeType, tempor.Height));
-			for (int i = 0; i < flights.Count; i++)
-			{
-				string[] arr = flights.ToArray()[i].Split(',');
-				string output = arr[2] + " " + arr[5] + "->" + arr[6];
-				Label label = new Label();
-				label.Click += previousClicked;
-				label.Text = output;
-				label.Dock = DockStyle.Fill;
-				previousFlightsTable.RowCount = previousFlightsTable.RowCount + 1;
-				previousFlightsTable.Controls.Add(label, 0, previousFlightsTable.RowCount - 1);
-			}
-		}
-		private void addUpcomingFlight(List<string> flights)
-		{
-			RowStyle tempor = upcomingFlightsTable.RowStyles[0];
+                    if (currentLine[3] != userTransactions.ToArray()[i][4])
+                    {
+                        continue;
+                    }
+                    DateTime flightDateTime = DateTime.Parse(currentLine[2] + " " + currentLine[0]);
+                    //Flight must have already happened
+                    if (flightDateTime < DateTime.Now)
+                    {
+                        previousFlights.Add(String.Join(',', currentLine));
+                    }
+                    //Flight Is Upcoming
+                    else
+                    {
+                        currentFlights.Add(String.Join(',', currentLine));
+                    }
+                    break;
 
-			upcomingFlightsTable.RowStyles.Add(new RowStyle(tempor.SizeType, tempor.Height));
-			for (int i = 0; i < flights.Count; i++)
-			{
-				string[] arr = flights.ToArray()[i].Split(',');
-				string output = arr[2] + " " + arr[5] + "->" + arr[6];
-				Label label = new Label();
-				label.Click += upcomingClicked;
-				label.Text = output;
-				label.Dock = DockStyle.Fill;
+                }
+                streamReaderA.Close();
+            }
+            addPreviousFlight(previousFlights);
+            addUpcomingFlight(currentFlights);
+            streamReaderA.Close();
+        }
 
-				upcomingFlightsTable.RowCount = upcomingFlightsTable.RowCount + 1;
-				upcomingFlightsTable.Controls.Add(label, 0, upcomingFlightsTable.RowCount - 1);
-			}
+        private void addPreviousFlight(List<string> flights)
+        {
 
-		}
+            RowStyle tempor = previousFlightsTable.RowStyles[0];
+      
+            for (int i = 0; i < flights.Count; i++)
+            {
+                previousFlightsTable.RowStyles.Add(new RowStyle(tempor.SizeType, tempor.Height));
+                string[] arr = flights.ToArray()[i].Split(',');
+                string output = arr[2] + " " + arr[5] + "->" + arr[6];
+                Label label = new Label();
+                label.Click += previousClicked;
+                label.Text = output;
+                label.Dock = DockStyle.Fill;
+                previousFlightsTable.RowCount = previousFlightsTable.RowCount + 1;
+                previousFlightsTable.Controls.Add(label, 0, previousFlightsTable.RowCount - 1);
+            }
+        }
+        private void addUpcomingFlight(List<string> flights)
+        {
+            RowStyle tempor = upcomingFlightsTable.RowStyles[0];
 
-		private void upcomingClicked(object sender, EventArgs e)
-		{
-			Label l = sender as Label;
-			int row = upcomingFlightsTable.GetRow(l);
+            
+            for (int i = 0; i < flights.Count; i++)
+            {
+                upcomingFlightsTable.RowStyles.Add(new RowStyle(tempor.SizeType, tempor.Height));
+                string[] arr = flights.ToArray()[i].Split(',');
+                string output = arr[2] + " " + arr[5] + "->" + arr[6];
+                Label label = new Label();
+                label.Click += upcomingClicked;
+                label.Text = output;
+                label.Dock = DockStyle.Fill;
 
-		}
+                upcomingFlightsTable.RowCount = upcomingFlightsTable.RowCount + 1;
+                upcomingFlightsTable.Controls.Add(label, 0, upcomingFlightsTable.RowCount - 1);
+            }
 
-		private void previousClicked(object sender, EventArgs e)
-		{
-			Label l = sender as Label;
-			int row = previousFlightsTable.GetRow(l);
-		}
-	}
+        }
+
+        private void upcomingClicked(object sender, EventArgs e)
+        {
+            Label l = sender as Label;
+            int row = upcomingFlightsTable.GetRow(l);
+
+        }
+
+        private void previousClicked(object sender, EventArgs e)
+        {
+            Label l = sender as Label;
+            int row = previousFlightsTable.GetRow(l);
+        }
+
+        private void clearTable(TableLayoutPanel table)
+        {
+
+
+            if (table.RowCount > 1)
+            {
+                while (table.RowCount != 1)
+                {
+                    int i = table.RowCount - 1;
+                    table.Controls.Remove(table.GetControlFromPosition(0, i));
+                    table.RowStyles.RemoveAt(i);
+                    table.RowCount--;
+
+                }
+            }
+
+        }
+
+        private void manageFlightsLoade(object sender, EventArgs e)
+        {
+            clearTable(upcomingFlightsTable);
+            clearTable(previousFlightsTable);
+            currentFlights = new List<string>();
+            previousFlights = new List<string>();
+            userTransactions = new List<string[]>();
+            initializeFlightList();
+        }
+    }
 }
