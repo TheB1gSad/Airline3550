f2ec29702c9983bc49e407adfdd2aec34eaebf23
Noah <ndougla2@rockets.utoledo.edu>
2023-04-29 14:48:48 -0400

    Fixed accounting manager; returns a single array with all relevant data and correctly locates csv files

--- Airline3550/Airline3550/AccountingManager.cs
+++ Airline3550/Airline3550/AccountingManager.cs
@@ -12,77 +12,81 @@ namespace Airline3550
 {
     internal class AccountingManager
     {
-        public List<string[]> getTotals()
+        public string[,] getFlightData()
         {
-            // Set the path of the CSV files
-            string flightListFilePath = @"csv\flightList.csv";
-            string flightSeatsFilePath = @"csv\flightSeats.csv";
+            // Path to the flightList.csv file
+            string flightListPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "csv", "flightList.csv");
 
-            // Read the flightList.csv file and store the flight prices in a dictionary
-            Dictionary<string, int> flightPrices = new Dictionary<string, int>();
+            // Read all lines from the flightList.csv file
+            string[] flightListLines = File.ReadAllLines(flightListPath);
 
-            using (var reader = new StreamReader(flightListFilePath))
+            // Create an array to store flight ID, price, seat information, percentage of seats filled, and cumulative revenue
+            string[,] flightData = new string[flightListLines.Length, 6];
+
+            // Loop through each line of the flightList.csv file
+            for (int i = 0; i < flightListLines.Length; i++)
             {
-                // Skip the header row
-                reader.ReadLine();
+                // Split the line by comma
+                string[] fields = flightListLines[i].Split(',');
 
-                // Loop through each line of the file
-                while (!reader.EndOfStream)
-                {
-                    // Split the line into its comma-separated values
-                    string[] values = reader.ReadLine().Split(',');
+                // Extract flight ID and price from the fields array
+                string flightID = fields[3];
+                string price = fields[8];
 
-                    // Get the flightID and price values
-                    string flightID = values[3];
-                    int price = int.Parse(values[8]);
-
-                    // Add the flight price to the dictionary
-                    flightPrices.Add(flightID, price);
-                }
+                // Add flight ID and price to the flightData array
+                flightData[i, 0] = flightID;
+                flightData[i, 1] = price;
             }
 
-            // Read the flightSeats.csv file and calculate the total amount of money made from each flightID
-            // and the percentage of how full each flight was
-            List<string[]> results = new List<string[]>();
+            // Path to the flightSeats.csv file
+            string flightSeatsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "csv", "flightSeats.csv");
 
-            using (var reader = new StreamReader(flightSeatsFilePath))
+            // Read all lines from the flightSeats.csv file
+            string[] flightSeatsLines = File.ReadAllLines(flightSeatsPath);
+
+            // Create a dictionary to store the number of taken seats and total seats for each flight
+            var seatData = new Dictionary<string, (int takenSeats, int totalSeats)>();
+
+            // Loop through each line of the flightSeats.csv file
+            foreach (string line in flightSeatsLines)
             {
-                // Skip the header row
-                //reader.ReadLine();
+                // Split the line by comma
+                string[] fields = line.Split(',');
 
-                // Loop through each line of the file
-                while (!reader.EndOfStream)
-                {
-                    // Split the line into its comma-separated values
-                    string[] values = reader.ReadLine().Split(',');
+                // Extract the flight ID and seat data from the fields array
+                string flightID = fields[0];
+                int[] seats = fields.Skip(1).Select(int.Parse).ToArray();
 
-                    // Get the flightID and the number of taken seats
-                    string flightID = values[0];
-                    int numTakenSeats = 0;
+                // Count the number of taken seats and total seats
+                int takenSeats = seats.Count(s => s != 0);
+                int totalSeats = seats.Length;
 
-                    for (int i = 1; i < values.Length; i++)
-                    {
-                        if (int.Parse(values[i]) != 0)
-                        {
-                            numTakenSeats++;
-                        }
-                    }
-
-                    // Calculate the total amount of money made from the flight
-                    int totalAmount = flightPrices[flightID] * numTakenSeats;
-
-                    // Calculate the percentage of how full the flight was
-                    double percentageFull = ((double)numTakenSeats / (double)(values.Length - 1)) * 100.0;
-
-                    // Add the flightID, total amount, and percentage full to the results list
-                    string[] result = new string[3];
-                    result[0] = flightID;
-                    result[1] = totalAmount.ToString();
-                    result[2] = percentageFull.ToString("0.00") + "%";
-                    results.Add(result);
-                }
-                return results;
+                // Add the seat data to the seatData dictionary
+                seatData[flightID] = (takenSeats, totalSeats);
             }
+
+            double cumulativeRevenue = 0;
+
+            // Add the seat data, calculate the percentage of seats filled, and cumulative revenue for each flight
+            for (int i = 0; i < flightData.GetLength(0); i++)
+            {
+                string flightID = flightData[i, 0];
+                (int takenSeats, int totalSeats) = seatData.GetValueOrDefault(flightID, (0, 0));
+
+                flightData[i, 2] = $"{takenSeats}/{totalSeats}";
+
+                double price = 0;
+                double.TryParse(flightData[i, 1], out price);
+
+                // Calculate the percentage of seats filled
+                double percentageFilled = totalSeats > 0 ? (double)takenSeats / totalSeats * 100 : 0;
+                flightData[i, 3] = $"{percentageFilled}%";
+
+                // Calculate the cumulative revenue
+                cumulativeRevenue += price * takenSeats;
+                flightData[i, 4] = cumulativeRevenue.ToString();
+            }
+            return flightData;
         }
     }
 
